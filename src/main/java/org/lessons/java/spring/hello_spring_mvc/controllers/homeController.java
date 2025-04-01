cebfee595e8ef62e7e47b178b0f83c3935622883
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-04-01 18:43:27 +0200

    feat: prova nuovi comandi

--- src/main/java/org/lessons/java/spring/hello_spring_mvc/controllers/homeController.java
+++ src/main/java/org/lessons/java/spring/hello_spring_mvc/controllers/homeController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -18,12 +20,14 @@ public class homeController {
     public String home() {
         return "home";
     }
-
-    @GetMapping("/prove")
+    
+    
+    @GetMapping("/${id}/prove")
     public String prendiDati(@RequestParam(name = "name") String name,
         @RequestParam String surname,
         @RequestParam String email,
         @RequestParam String tel,
+        @PathVariable String id,
          Model model) {
 
         model.addAttribute("name", name);
@@ -31,8 +35,13 @@ public class homeController {
         model.addAttribute("email", email);
         model.addAttribute("tel", tel);
         model.addAttribute("curDate", LocalDate.now());
+        model.addAttribute("id", id);
+
         return "prove";
     }
+
     
+
+   
     
 }
